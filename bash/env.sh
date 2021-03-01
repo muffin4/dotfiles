f8a06aab00f929fafbfbf063b161907c25e1d754
isa <isa4@posteo.de>
2021-03-01 13:16:27 +0100

    ripgreprc: ignore .git

--- bash/env.sh
+++ bash/env.sh
@@ -1,6 +1,7 @@
 export VISUAL=kak
 [ "$TERM" = "xterm" ] && export TERM=xterm-256color
 export _JAVA_OPTIONS='-Dawt.useSystemAAFontSettings=on -Dswing.aatext=true -Dswing.defaultlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel'
-export SSH_AUTH_SOCK="${XDG_RUNTIME_DIR}/ssh-agent.socket"
 export LESSHISTFILE=/dev/null
+export RIPGREP_CONFIG_PATH="${XDG_CONFIG_HOME:-"$HOME/.config"}/ripgreprc"
+export SSH_AUTH_SOCK="${XDG_RUNTIME_DIR}/ssh-agent.socket"
 export XZ_DEFAULTS=--threads=0
