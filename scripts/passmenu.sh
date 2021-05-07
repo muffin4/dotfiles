6fe13bcdcc8d40c7fc0dbf0063b101ffa0f178ba
isa <isa4@posteo.de>
2021-05-07 16:43:58 +0200

    passmenu: convert to a bash script
    that closes kitty once fzf is complete by using named fifos

--- scripts/passmenu.sh
+++ scripts/passmenu.sh
@@ -1,2 +1,41 @@
-#! /bin/sh
-exec kitty --name=Passmenu --title=Passmenu sh -c "$HOME/.scripts/passmenu.py $*"
+#! /usr/bin/env bash
+set -e
+
+line=1
+
+usage() {
+    echo "Usage: $0 [OPTION]..."
+    echo
+    echo "  -h show this help text"
+    echo "  -u copy username instead of password"
+    exit 1
+}
+
+
+while getopts hu option
+do
+    case $option in
+        u) line=2;;
+        *) usage;;
+    esac
+done
+
+tmppipe=$(mktemp --dry-run)
+mkfifo --mode=600 "$tmppipe"
+
+"$TERMINAL" bash -c "\
+    shopt -s globstar
+    prefix=\${PASSWORD_STORE_DIR:-\${HOME}/.password-store}
+    files=( \"\$prefix\"/**/*.gpg )
+    args=()
+    for file in \"\${files[@]}\"
+    do
+        file=\${file%.gpg}
+        file=\${file#\${prefix}/}
+        args+=(\"\$file\")
+    done
+    printf \"%s\0\" \"\${args[@]}\" | fzf --read0 > $(printf %q "$tmppipe")
+" & selection=$(cat "$tmppipe")
+
+rm "$tmppipe"
+pass show -c"$line" "$selection"
