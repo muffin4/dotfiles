c71113f7ebdf1e2c300a68337c9fb675df6d0e7d
Isa Cichon <isa.ci@posteo.de>
2021-10-07 14:28:54 +0200

    kitty: add script to toggle between light and dark mode

---
+++ kitty/toggle-light-dark.sh
@@ -0,0 +1,25 @@
+#! /bin/sh
+set -e
+
+dark=themes/Tango_Dark.conf
+light=themes/Tango_Light.conf
+target=theme.conf
+
+if ! [ -a "$target" ]; then
+	ln -vs "$light" "$target"
+	exit 0
+fi
+
+if ! [ -h "$target" ]; then
+	echo "$target is not a symbolic link"
+	exit 1
+fi
+
+if [ "$target" -ef "$dark" ]; then
+	ln -vsf "$light" "$target"
+elif [ "$target" -ef "$light" ]; then
+	ln -vsf "$dark" "$target"
+else
+	echo "unexpected link in $target" >&2
+	exit 1
+fi
