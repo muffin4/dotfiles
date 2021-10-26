0678213b0f13560e9fc21cf2a8daa0ecc8ef60a2
Isa Cichon <isa.ci@posteo.de>
2021-10-26 14:14:27 +0200

    tmux: used XDG_RUNTIME_DIR for tmux socket
    instead of world writable /tmp

--- bash/env.sh
+++ bash/env.sh
@@ -6,5 +6,6 @@ export EDITOR=$VISUAL
 export _JAVA_OPTIONS='-Dawt.useSystemAAFontSettings=on -Dswing.aatext=true -Dswing.defaultlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel'
 export LESSHISTFILE=/dev/null
 export RIPGREP_CONFIG_PATH="${XDG_CONFIG_HOME:-"$HOME/.config"}/ripgreprc"
+export TMUX_TMPDIR="${XDG_RUNTIME_DIR}"
 export SSH_AUTH_SOCK="${XDG_RUNTIME_DIR}/ssh-agent.socket"
 export XZ_DEFAULTS=--threads=0
