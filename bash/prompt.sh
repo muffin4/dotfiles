7a7293a8afba10da344d54887f14ace85b56f35c
Isa Cichon <isa.ci@posteo.de>
2023-05-14 10:37:42 +0200

    bash: add last exit code to prompt if not 0

--- bash/prompt.sh
+++ bash/prompt.sh
@@ -1,2 +1,15 @@
 VIRTUAL_ENV_DISABLE_PROMPT=y
-PS1="; "
+
+PROMPT_COMMAND=__prompt_command
+__prompt_command() {
+	local EXIT=$?
+	PS1=""
+
+	if [[ $EXIT != 0 ]]; then
+		PS1+="$EXIT "
+	fi
+
+	PS1+="; "
+}
+
+#PS1="; "
