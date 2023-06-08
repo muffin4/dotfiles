a5e12406387486a38a68c2d7d6f75999e7c86676
Isa Cichon <isa.ci@posteo.de>
2023-06-08 11:03:19 +0200

    bash: add "view" alias
    to open with vim (or nvim) in read-only mode

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -19,6 +19,7 @@ alias bc="bc --mathlib --quiet"
 alias hibernate="systemctl hibernate"
 alias v=vim
 if command -v nvim >/dev/null; then alias vim=nvim; fi
+alias view="vim -R"
 alias o="shtart xdg-open"
 
 alias tmuxdotties='tmux new -c ~/dotfiles -s dotties -A'
