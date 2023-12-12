fa6ccd04c2b2c909271141cf65658dfb652011b9
Isa Cichon <isa.ci@posteo.de>
2023-12-12 17:36:39 +0100

    bash: alias dirs='dirs -v'
    print the directory stack with one entry per line prefixed with its position in the stack

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -1,12 +1,16 @@
 alias ls='LC_ALL=C ls --color=auto --group-directories-first'
 alias l='ls -lh'
 alias la='ls -lhA'
-alias date_clean='date +%F_%H-%M-%S'
-alias mv='mv --interactive'
-alias cp='cp --interactive'
-alias tuxsay='cowsay -f tux'
 alias sl='ls'
 alias ..='cd ..'
+alias mv='mv --interactive'
+alias cp='cp --interactive'
+
+# print the directory stack with one entry per line prefixed with its position in the stack
+alias dirs='dirs -v'
+
+alias date_clean='date +%F_%H-%M-%S'
+alias tuxsay='cowsay -f tux'
 alias steamlib='find ~/.steam/root/ \( -name "libgpg-error.so*" -o -name "libstdc++.so*" \) -print -delete'
 alias py='python'
 alias pamcan=pacman
@@ -26,6 +30,7 @@ alias trash="gio trash"
 alias tmuxdotties='tmux new -c ~/dotfiles -s dotties -A'
 alias tmuxpass='tmux new -c ~/.password-store -s pass -A'
 alias tmuxjornal='tmux new -c ~/.jornal -s jornal -A'
+
 alias sed4='sed -e "1s/..../& /g"'
 
 type _complete_alias &>/dev/null && while read line; do
