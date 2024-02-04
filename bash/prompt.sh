1d03e33083008c45f20029afdd3b6b0d8fa3a9c7
Isa Cichon <isa.ci@posteo.de>
2024-02-04 15:58:32 +0100

    bash: display (in ranger) when shell is in ranger

--- bash/prompt.sh
+++ bash/prompt.sh
@@ -1,4 +1,10 @@
 __PS1='; '
+
+# Based on /usr/share/doc/ranger/examples/shell_subshell_notice.sh
+# Compatible with ranger 1.5.3 through 1.9.*
+# Change the prompt when you open a shell from inside ranger
+if [ -n "$RANGER_LEVEL" ]; then __PS1="(in ranger) ${__PS1}"; fi
+
 PS1=$__PS1
 
 # Allow parameter expansion, command substitution, arithmetic expansion, and quote removal in prompt strings.
