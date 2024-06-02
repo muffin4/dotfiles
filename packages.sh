31659632e8f850926b6d9e766138efe7c4bd9eb9
Isa Cichon <71238026+muffin4@users.noreply.github.com>
2024-06-02 11:43:04 +0200

    mpd: install mpd and ario
    MPD is a daemon for playing music
    ario is a graphical GTK2 client for MPD

--- packages.sh
+++ packages.sh
@@ -60,6 +60,9 @@ programs=(
     bat # a cat clone with syntax highlighting and Git integration
     ranger # terminal file browser
 
+    mpd # a daemon for playing music
+    ario # a GTK2 client for MPD
+
     openttd # OpenTTD is an open source simulation game based upon the popular Microprose game "Transport Tycoon Deluxe", written by Chris Sawyer. It attempts to mimic the original game as closely as possible while extending it with new features. It has trains.
     openttd-opengfx openttd-opensfx # open graphics and sounds for OpenTTD
 )
