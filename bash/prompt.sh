39fb0ef2a11e59dd76a7edd034969e34ffccb1ed
Isa Cichon <71238026+muffin4@users.noreply.github.com>
2025-11-23 14:44:55 +0100

    bash: remove non-zero exit code from prompt
    by removing the prompt command entirely to get rid of this error message
    after each command when using bash inside tmux:
    
    -bash: systemd_osc_context_cmd_id: unbound variable

--- bash/prompt.sh
+++ bash/prompt.sh
@@ -6,19 +6,3 @@ __PS1='; '
 if [ -n "$RANGER_LEVEL" ]; then __PS1="(in ranger) ${__PS1}"; fi
 
 PS1=$__PS1
-
-# Allow parameter expansion, command substitution, arithmetic expansion, and quote removal in prompt strings.
-shopt -s promptvars
-
-PROMPT_COMMAND=__prompt_command
-__prompt_command() {
-	local EXIT=$?
-	PS1=""
-
-	if [[ $EXIT != 0 ]]; then
-		PS1+="$EXIT "
-	fi
-
-	# include default
-	PS1+=$__PS1
-}
